ce65c628ee8d4c0611421a46568ec9beb912f8c9
tomerzibman <tomerzibman@gmail.com>
2024-04-21 19:30:24 -0400

    made sure to return user photo when returning conversations

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -5,16 +5,28 @@ const getConversationsForUser = async (req, res) => {
   console.log("req.params.id: ", req.params.id);
   const conversations = await Conversation.find({
     participants: { $in: [req.params.id] },
-  }).populate("participants", "username photo");
+  }).populate("participants", "id username photo");
   res.json(conversations);
 };
 
 const createConversation = async (req, res) => {
+  const { participants } = req.body;
+  const existingConversation = await Conversation.findOne({
+    participants: { $all: participants },
+  });
+
+  if (existingConversation) {
+    return res.status(400).json({ error: "Conversation already exists" });
+  }
+
   const conversation = new Conversation({
-    participants: req.body.participants,
+    participants: participants,
   });
   const savedConversation = await conversation.save();
-  res.json(savedConversation);
+  const populatedConversation = await Conversation.findById(
+    savedConversation._id
+  ).populate("participants", "id username photo");
+  res.json(populatedConversation);
 };
 
 module.exports = { getConversationsForUser, createConversation };
